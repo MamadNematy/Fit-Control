10acc2dacfeb0476d240016123f9dc23c56147d9
MamadNematy <mj2864250@gmail.com>
2025-05-22 12:51:26 +0330

    fix responsive bugs

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.getElementById("theme-toggle");
 
-  // بررسی حالت ذخیره شده در localStorage
   if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark-theme");
   }
@@ -9,7 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
   toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme");
 
-    // ذخیره حالت در localStorage
     if (document.body.classList.contains("dark-theme")) {
       localStorage.setItem("theme", "dark");
     } else {
